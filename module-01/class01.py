7f398ffa479ab309f44f4c963cb8012ace27de05
systembyzihad <systembyzihad@gmail.com>
2026-05-25 18:28:21 +0600

    continue

--- module-01/class01.py
+++ module-01/class01.py
@@ -1,1 +1,22 @@
-print("hello world")
+# print("hello world")
+
+# x=10
+# print(type(x))
+
+# y='hello'
+# print (type(y))
+
+# print ("enter your name: ")
+# name = input()
+# print("nice to meet you,",name)
+# age=input("enter your age: ")
+# print("you are",age,"years old")
+# print("you are " + age + " years old")
+
+num_int=10
+num_float=10.5
+num_new=int(num_int+num_float)
+print("the value of num_new is:", num_new)
+print("the datatype of num_int is:", type(num_int))
+print("the datatype of num_float is:", type(num_float))
+print("the datatype of num_new is:", type(num_new))
