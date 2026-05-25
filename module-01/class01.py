80c1b4c47ba964c6971d6485730348494e979bc8
systembyzihad <systembyzihad@gmail.com>
2026-05-25 20:21:00 +0600

    done class 1

--- module-01/class01.py
+++ module-01/class01.py
@@ -13,10 +13,38 @@
 # print("you are",age,"years old")
 # print("you are " + age + " years old")
 
-num_int=10
-num_float=10.5
-num_new=int(num_int+num_float)
-print("the value of num_new is:", num_new)
-print("the datatype of num_int is:", type(num_int))
-print("the datatype of num_float is:", type(num_float))
-print("the datatype of num_new is:", type(num_new))
+# num_int=10
+# num_float=10.5
+# num_new=int(num_int+num_float)
+# print("the value of num_new is:", num_new)
+# print("the datatype of num_int is:", type(num_int))
+# print("the datatype of num_float is:", type(num_float))
+# print("the datatype of num_new is:", type(num_new))
+
+# new_int=27
+# new_str="456"
+# new_sum=str(new_int)+new_str
+# print(new_sum)
+# print("type of new_int is:", type(new_int))
+# print("type of new_str is:", type(new_str))
+# print("type of new_sum is:", type(new_sum))
+
+# a=True
+# b=False
+# print("the value of a is:", a)
+# print("the value of b is:", b)
+# print("the type of a is:", type(a))
+# print("the type of b is:", type(b))
+
+a=5
+b=2
+# temp=a
+# a=b
+# b=temp
+a,b=b,a
+
+print("the value of a is:", a)
+print("the value of b is:", b)
+
+x="hello"
+print(x[0:3])
