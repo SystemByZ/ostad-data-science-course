af2174c9a8a1e2473d7b3c2bc9e9b63303a471d0
systembyzihad <systembyzihad@gmail.com>
2026-05-30 06:05:29 +0600

    module 1 done here

--- module-01/class02.py
+++ module-01/class02.py
@@ -1,7 +1,16 @@
+# n=1
+# sum=0
+# while n<=100:
+#     sum=sum+n
+#     n=n+2
+# print("the sum of odd numbers from 1 to 100 is:", sum)
 
-n=1
-sum=0
-while n<=100:
-    sum=sum+n
-    n=n+2
-print("the sum of odd numbers from 1 to 100 is:", sum)
+
+# aList = [4, 8, 12, 16]
+
+# aList[1:4] = [20, 24, 28]
+
+# print(aList)
+for num in range(-2,-5,-1):
+
+    print(num, end=", ")
