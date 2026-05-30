03b26601471bcb9130294a102df8c95a895f3857
systembyzihad <systembyzihad@gmail.com>
2026-05-30 07:27:02 +0600

    module 2 start

---
+++ module-02/class-01.py
@@ -0,0 +1,27 @@
+# Topic:List, Tuple, Set, Dictionary
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist)
+# # print(thislist[1])
+# # print(thislist[-1])
+# # print(thislist[2:5])
+# print(thislist[:4])
+# # print(thislist[2:])   
+# print(thislist[-1:-4:-1])    
+
+# list1 = ["apple", "banana", "cherry"]
+# list2 = [1, 5, 7, 9, 3]
+# list3 = [True, False, False]
+# print(list3)
+# list4 = ['abc', 34, True, 40, "male"]
+# print(list4)
+
+# list1 = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(list1)
+# print(list1+["tomato", "grape",140])
+# print(list1, ["tomato", "grape",140])
+# print(list1*4)
+# print(list1[2])
+# print(list1[-1:-8:-1])
+fruits = ["apple", "banana", "cherry", "orange"]
+fruits.append("kiwi")
+print(fruits)
