a7b5f902a6bba65eeed139d4e1dd947e966e0864
systembyzihad <systembyzihad@gmail.com>
2026-06-06 18:08:20 +0600

    list and tuple done

--- module-02/class-01.py
+++ module-02/class-01.py
@@ -1,12 +1,15 @@
 # Topic:List, Tuple, Set, Dictionary
-# thislist = ["apple", "banana", "cherry"]
-# print(thislist)
-# # print(thislist[1])
-# # print(thislist[-1])
-# # print(thislist[2:5])
-# print(thislist[:4])
-# # print(thislist[2:])   
-# print(thislist[-1:-4:-1])    
+
+
+
+# this_list = ["apple", "banana", "cherry"]
+# print(this_list)
+# # print(this_list[1])
+# # print(this_list[-1])
+# # print(this_list[2:5])
+# print(this_list[:4])
+# # print(this_list[2:])   
+# print(this_list[-1:-4:-1])    
 
 # list1 = ["apple", "banana", "cherry"]
 # list2 = [1, 5, 7, 9, 3]
@@ -22,8 +25,6 @@
 # print(list1*4)
 # print(list1[2])
 # print(list1[-1:-8:-1])
-fruits = ["apple", "banana", "cherry", "orange","cherry","cherry"]
-# fruits.append("kiwi")
 # print(fruits)
 # fruits.insert(1, "lemon")
 # print(fruits)
@@ -50,7 +51,123 @@ fruits = ["apple", "banana", "cherry", "orange","cherry","cherry"]
 # print(y)
 # print(z)
 # print(fruits)
-fruits.remove("cherry")
-fruits.remove("cherry")
-fruits.remove("cherry")
-print(fruits)
+# fruits.remove("cherry")
+# fruits.remove("cherry")
+# fruits.remove("cherry")
+# print(fruits)
+# print(len(fruits))
+
+# A=[[1,2,3,4],[4,5,6,7],[4,7,8,9]]
+# print(A)
+# print(A[0])
+# print(A[1])
+# print(A[2])     
+# print(A[0][0])
+# print(A[0][1])
+# print(A[0][2])
+# print(A[0][3])
+# print(A[1][0],end=" ") 
+# print(A[1][1],end=" ")
+# print(A[1][2],end=" ")
+# print(A[1][3])   
+# print(A[2][0],end=" ")
+# print(A[2][1],end=" ")
+# print(A[2][2],end=" ")
+# print(A[2][3])   
+
+# column = [[2],[5],[7]]
+# for row in A:
+#     column.insert(0,row[1])
+# print(column)
+
+
+# tuples start here
+
+# this_tuple = ("apple", "banana", "cherry")
+# print(this_tuple)
+
+# fruits = ("apple", "banana", "cherry")
+# print(fruits)
+# (green, yellow, red) = fruits
+# print(green)
+# print(yellow)
+# print(red)
+
+#For loop practice
+
+# sum=0
+# for i in range(0,101):
+#     if i%2==0:
+#         sum=sum+i
+# print(sum)
+# this_list = ["apple", "banana", "cherry"]
+
+# for fruits in this_list:
+#     print(fruits )
+#     for i in fruits:
+        
+#         if i=="r":
+#             continue
+#         print(i ,end="")
+#         print(" ")
+
+# print("This is the end of the program")
+
+
+# n=int(input("Enter a number:",))
+# if n % 2 != 0:
+#     print("Weird")
+# elif (n%2==0 and 1<n<6):
+#     print("Not Weird")
+# elif (n%2==0 and 5<n<21):
+#     print("Weird")
+# elif (n%2==0 and 20<n):
+#     print("Not Weird")
+
+# A=[[1,2,3,4],[4,5,6,7],[4,7,8,9]]
+# print(A)
+# print("end")
+# column=[]
+# for row in A:
+#     column.append(row[1])
+# print(column)
+# for i in column:
+#     print([i])
+
+# for i in column:
+#     for j in A:
+#         if i in j:
+#             print(j)
+
+#Tuples start here
+# this_tuple = ("apple","banana","cherry")
+# print(this_tuple)
+
+# fruits = ("apple", "banana", "cherry")
+# print(fruits)
+# (red,green,yellow)=fruits
+# print(green)
+# print(yellow)
+# print(red)
+# fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+# print(fruits)
+# (red,green,*blue)=fruits
+# print(red)
+# print(green)
+# print(blue)#*blue is used to store the remaining values in the tuple after assigning the first two values to red and green. In this case, blue will contain the values "cherry", "orange", "kiwi", "melon", and "mango".it represents a list of the remaining values in the tuple after the first two values have been assigned to red and green.
+# a=(5,6)
+# b=(8,4)
+# if a>b:
+#     print("a is bigger")
+# else: 
+#     print("b is bigger")
+
+# A=[(5,6),(8,4),(7,9)]
+# B=sorted(A)
+# print(B)
+a=(0,3,6,5)
+b=(10,2,5,2)
+if a<b:
+    print("b is bigger")
+else:
+    print("a is bigger")
