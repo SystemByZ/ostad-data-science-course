a9f312418f5577a1448f8bace2ca53d6faf35522
systembyzihad <systembyzihad@gmail.com>
2026-07-31 09:44:40 +0600

    numpy random

---
+++ projects/number_guessing_game.py
@@ -0,0 +1,15 @@
+import random 
+secret = random.randint(1,100)
+while True:
+   guess = int(input("Guess a number between 1 and 100:"))
+
+   if guess < secret:
+      print("Too low")
+
+   elif guess > secret:
+      print("Too high")
+
+   else:
+      print("Congrats, you got it right!")
+
+   continue;
